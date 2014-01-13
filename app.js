bcfcbdb8c2689cdbeb47d9e40f0189352c7d0b2e
yanghua <yanghua1127@gmail.com>
2014-01-13 15:57:50 +0800

    make some changes for express version from 2.x to 3.x

--- app.js
+++ app.js
@@ -22,3 +22,88 @@
   Desc: the enter of the project
  */
 
+var fs           = require("fs");
+var path         = require("path");
+var express      = require("express");
+var router       = require("./router");
+var common       = require("./common/error");
+var AppConfig    = require("./config").config;
+var Loader       = require("loader");
+var errorHandler = require("./common/errorHandler");
+
+var cronService  = require("./service/cron");
+
+var assets = {};
+if (AppConfig.mini_assets) {
+    try {
+        assets = JSON.parse(fs.readFileSync(path.join(__dirname, 'assets.json')));
+    } catch (e) {
+        console.log('You must execute `make build` before start app when mini_assets is true.');
+        throw e;
+    }
+}
+
+var app          = express();
+
+//config for all env
+app.configure(function () {
+    app.set('view engine', 'html');
+    app.set('views', path.join(__dirname, 'views'));
+    app.set("view options", {layout : false});
+    app.engine('html', require('ejs').renderFile);
+
+    //middleware
+    app.use(express.compress());
+    app.use(express.favicon());
+    
+    app.use(express.query());
+    app.use(express.bodyParser({ uploadDir : "./uploads"}));
+
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret : AppConfig.session_secret,
+        cookie : {
+            maxAge  : 30 * 60 * 1000      //ms
+        }
+    }));
+});
+
+//custom middleware
+// app.use(require("./controllers/login").commonProcess);
+
+var maxAge = 3600000 * 24 * 30;
+var staticDir = path.join(__dirname, 'public');
+
+//set static, dynamic helpers
+app.locals({
+    config: AppConfig,
+    Loader: Loader,
+    assets: assets
+});
+
+//config for devp env
+app.configure('development', function () {
+    app.use(express.logger());
+    app.use("/public", express.static(staticDir));
+    app.use(express.errorHandler(
+        { showStack : true, dumpException : true }
+    ));
+});
+
+//config for production env
+app.configure("production", function () {
+    app.use('/public', express.static(staticDir, { maxAge: maxAge }));
+    app.use(express.errorHandler());
+    app.set('view cache', true);
+});
+
+//error hanlder
+errorHandler.appErrorProcess(app);
+
+router(app);
+
+//launch it!
+app.listen(AppConfig.port);
+console.log("the app server run at port :%d in %s mode. ", AppConfig.port, app.settings.env);
+
+module.exports = app;
