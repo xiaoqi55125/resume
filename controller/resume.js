b39e565b7ae81f2c376acf7a057b6aa7a98569f9
yanghua <yanghua1127@gmail.com>
2014-01-16 18:29:40 +0800

    add a log

--- controller/resume.js
+++ controller/resume.js
@@ -98,6 +98,7 @@ exports.upload = function (req, res, next) {
     var mainAnalysisScript = path.resolve(__dirname, "../", config.analysis_mainscript_path);
     var result = execSync.exec("python {0}".format(mainAnalysisScript));
 
+    debugCtrller(results.stdout);
     debugCtrller(fs.existsSync(result.stdout));
     return res.send(resUtil.generateRes(null, config.statusCode.STATUS_OK));
 
