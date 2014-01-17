59b92d4a1f691cdce4ad129a11e8381737295809
yanghua <yanghua1127@gmail.com>
2014-01-17 14:50:25 +0800

    changed handle file style

--- controller/resume.js
+++ controller/resume.js
@@ -103,9 +103,11 @@ exports.upload = function (req, res, next) {
             debugCtrller("step pipeHtmlFile");
             var ext = path.extname(fileName);
             if (ext.indexOf("htm") != -1 || ext.indexOf("html") != -1) {
-                var htmlStream = fs.createReadStream(uploadFilePath);
-                var newHtmlStream = fs.createWriteStream(transferFilePath);
-                htmlStream.pipe(newHtmlStream, { encoding : 'utf8'});
+                // var htmlStream = fs.createReadStream(uploadFilePath);
+                // var newHtmlStream = fs.createWriteStream(transferFilePath);
+                // htmlStream.pipe(newHtmlStream, { encoding : 'utf8'});
+                var content = fs.readFileSync(uploadFilePath, { encoding : "utf8" });
+                fs.writeFileSync(transferFilePath, content, { encoding : "utf8" });
                 return callback(null, null);
             } else if (ext.indexOf("zip") != -1) {
                 var uncompressPath = path.resolve(__dirname, "../", config.uncompress_file_path);
