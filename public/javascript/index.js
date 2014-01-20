d5ee3818a4c90924854d6a2da7ed82bd9059593c
Zhicheng Liu <xiaoqi55125@gmail.com>
2014-01-20 13:22:26 +0800

    test page

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -21,76 +21,81 @@
   Time: 1:35 PM
  */
 /**
- * do the resume search 
- * @return {null} 
+ * do the resume search
+ * @return {null}
  */
-function resumeSearch () {
-  //without check
-  $.ajax({
-    url:'/resume/query',
-    type:'POST',
-    data:$('form.resumeSearchForm').serialize(),
-    success:function (data) {
-      if (data.statusCode === 0) {
-        changeListView(data.data);
-        changeMainTextValue(data.data[0]);
-      }
-    }
-  })
+function resumeSearch(pageSize, pageIndex) {
+    //without check
+    $.ajax({
+        url: '/resume/query',
+        type: 'POST',
+        data: {
+            'pageSize': 10,
+            'pageIndex': 1
+        },
+        //data: $('form.resumeSearchForm').serialize(),
+        success: function(data) {
+            if (data.statusCode === 0) {
+                changeListView(data.data);
+                changeMainTextValue(data.data[0]);
+            }
+        }
+    })
 }
 
-function changeListView (data) {
-  $("#listView").html("");
-  for (var i = 0; i < data.length; i++) {
-    $("#resumeList").fadeIn(600);
-    var link = $("<a href='javascript:void(0);' class='list-group-item'>"+data[i].userName+"</a>");
-    link.click(tdCont.listClick(data[i]._id,data[i]));
-    $("#listView").append(link);
-  };
+function changeListView(data) {
+    $("#listView").html("");
+    for (var i = 0; i < data.length; i++) {
+        $("#resumeList").fadeIn(600);
+        var link = $("<a href='javascript:void(0);' class='list-group-item'>" + data[i].userName + "</a>");
+        link.click(tdCont.listClick(data[i]._id, data[i]));
+        $("#listView").append(link);
+    };
 }
 
-function changeMainTextValue (data) {
-  $("#uName").html(data.userName);
-  $("#sex").html(data.sex);
-  $("#mobile").html(data.mobile);
-  $("#birth").html(data.birthday);
-  $("#rFrom").html(data.source);
-  //college
-  $("#college").html(data.education.college);
-  $("#degree").html(data.education.degree);
-  $("#graduatedTime").html(data.education.graduatedTime);
-  $("#speciality").html(data.education.speciality);
+function changeMainTextValue(data) {
+    $("#uName").html(data.userName);
+    $("#sex").html(data.sex);
+    $("#mobile").html(data.mobile);
+    $("#birth").html(data.birthday);
+    $("#rFrom").html(data.source);
+    //college
+    $("#college").html(data.education.college);
+    $("#degree").html(data.education.degree);
+    $("#graduatedTime").html(data.education.graduatedTime);
+    $("#speciality").html(data.education.speciality);
 
-  //experience
-  $("#experienceAttr").html("");
-  for (var i = 0; i < data.experiences.length; i++) {
-      var cellData = data.experiences[i];
-      var cnt = i+1;
-      var temp  = "<tr><td>"+
-                cnt+"</td><td>"+
-                cellData.position+"</td><td>"+
-                cellData.company+"</td><td>"+
-                cellData.time+"</td></tr>";
-      $("#experienceAttr").append(temp);
-  }; 
+    //experience
+    $("#experienceAttr").html("");
+    for (var i = 0; i < data.experiences.length; i++) {
+        var cellData = data.experiences[i];
+        var cnt = i + 1;
+        var temp = "<tr><td>" +
+            cnt + "</td><td>" +
+            cellData.position + "</td><td>" +
+            cellData.company + "</td><td>" +
+            cellData.time + "</td></tr>";
+        $("#experienceAttr").append(temp);
+    };
 }
 
-function listClickFunc (id,data) {
-  $("#resumeDetail").hide();
-  $("#resumeDetail").fadeIn(600);
-  changeMainTextValue(data);
+function listClickFunc(id, data) {
+    $("#resumeDetail").hide();
+    $("#resumeDetail").fadeIn(600);
+    changeMainTextValue(data);
 }
+
 var tdCont = {
-  cell: function(item) {
-    return $("<td></td>").html(item);
-  },
+    cell: function(item) {
+        return $("<td></td>").html(item);
+    },
 
-  row: function() {
-    return $("<tr></tr>");
-  },
-  listClick: function(id,data) {
-    return function() {
-      listClickFunc(id,data);
+    row: function() {
+        return $("<tr></tr>");
+    },
+    listClick: function(id, data) {
+        return function() {
+            listClickFunc(id, data);
+        }
     }
-  }
 }
