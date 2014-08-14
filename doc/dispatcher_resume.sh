c796427145bffd53e96d9d74032be2181ce1ee14
yanghua <yanghua1127@gmail.com>
2014-08-14 10:28:58 +0800

    fixed some install problem

--- doc/dispatcher_resume.sh
+++ doc/dispatcher_resume.sh
@@ -10,6 +10,11 @@ sudo apt-get install convmv
 
 sudo apt-get install p7zip-full
 
+#for node_module -> canvas 
+sudo apt-get update
+
+sudo apt-get install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
+
 cd ~
 
 rm -R resume
@@ -38,6 +43,9 @@ make build
 
 #give the backup dir the other user write premission
 # sudo chmod o+w backup
+
+#global install pm2@0.6.5
+sudo npm install -g pm2@0.6.5
 
 pm2 kill
 #make sure you have setted 'NODE_ENV'
